d4939896a9e0e2da21b989382a4b534e10cc9072
blackmirrh <blackmirrh@gmail.com>
2018-10-12 08:13:59 -0400

    DeepCopy

---
+++ DataStructureAlgorithms/BST.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithms
+{
+    class BST
+    {
+    }
+
+    public class Node<T>
+    {
+        // Private member-variables
+        private T data;
+        private NodeList<T> neighbors = null;
+
+        public Node() { }
+        public Node(T data) : this(data, null) { }
+        public Node(T data, NodeList<T> neighbors)
+        {
+            this.data = data;
+            this.neighbors = neighbors;
+        }
+
+        public T Value
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        protected NodeList<T> Neighbors
+        {
+            get { return neighbors; }
+            set { neighbors = value; }
+        }
+    }
+
+    public class NodeList<T> : Collection<Node<T>>
+    {
+        public NodeList() : base() { }
+
+        public NodeList(int initialSize)
+        {
+            for(int i = 0; i < initialSize; i++)
+            {
+                base.Items.Add(default(Node<T>));
+            }
+        }
+
+        public Node<T> FindByValue(T value)
+        {
+            foreach (Node<T> node in Items)
+                if (node.Value.Equals(value))
+                    return node;
+
+            // if we reached here, we didn't find a matching node
+            return null;
+        }
+
+    }
+
+    public class BinaryTreeNode<T> : Node<T>
+    {
+        public BinaryTreeNode() : base() { }
+        public BinaryTreeNode(T data) : base(data, null) { }
+        public BinaryTreeNode(T data, BinaryTreeNode<T> left, BinaryTreeNode<T> right)
+        {
+            base.Value = data;
+            NodeList<T> children = new NodeList<T>(2);
+            children[0] = left;
+            children[1] = right;
+
+            //base.Neighbors
+        }
+
+    }
+}
---
+++ Misc/DeepCopy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misc
+{
+    [Serializable]
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+
+    [Serializable]
+    public class Employee:ICloneable
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public Department Department { get; set; }
+
+        public object Clone()
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                if(this.GetType().IsSerializable)
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(stream, this);
+                    stream.Position = 0;
+                    return formatter.Deserialize(stream);
+                }
+                return null;
+            }
+        }
+    }
+
+    public class DeepCopy
+    {
+    }
+}
